332b02cc7c46e3ae5492dabc2d4fca1d14b42421
florpalma1 <tuemail@example.com>
2024-12-10 11:56:12 -0300

    agregue formulario de resultados de partidos

--- src/components/PaginaEquipos.js
+++ src/components/PaginaEquipos.js
@@ -1,9 +1,11 @@
 import { FormularioEquipos } from "./FormularioEquipos"
+import { FormularioResultados } from "./FormularioResultados"
 
 export const PaginaEquipos = () => {
   return (
     <div>
         <FormularioEquipos/>
+        <FormularioResultados/>
     </div>
   )
 }
